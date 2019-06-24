55abea37b82838bc2c725cae6e5eb4d3f4265f38
ariefzainuri <ariefzainuri@kotalogue.com>
2019-06-24 13:18:03 +0700

    Initial Commit

---
+++ Test Apps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test Apps
+//
+//  Created by Arief Zainuri on 24/06/19.
+//  Copyright © 2019 Arief Zainuri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
